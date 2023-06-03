b7c28c47edf412f3a5f52324ab97640fa3ac3c0d
Timothy W Fortune <twfortune72@gmail.com>
2023-06-03 09:54:19 -0500

    Initial Commit

---
+++ M3L2-GitHub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M3L2-GitHub
+//
+//  Created by Timothy W Fortune on 6/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L2-GitHub/M3L2_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2_GitHubApp.swift
+//  M3L2-GitHub
+//
+//  Created by Timothy W Fortune on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct M3L2_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
